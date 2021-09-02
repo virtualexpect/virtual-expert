d949930c1a688f848938ae0e21e5cbab1fc6154c
virtualexpect <89941667+virtualexpect@users.noreply.github.com>
2021-09-02 16:16:56 +0600

    new file added

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "www.linkpicture.com",
+      "lh3.googleusercontent.com",
+      "media.istockphoto.com",
+      "i.pinimg.com",
+      "www.amzonestep.com",
+      "res.cloudinary.com",
+    ],
+  },
+};
